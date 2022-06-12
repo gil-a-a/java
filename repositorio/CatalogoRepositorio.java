8184ef718e7b3c83df28f74bcea54a36d9876fd1
gil-a-a <98371042+gil-a-a@users.noreply.github.com>
2022-06-11 22:04:54 -0300

    Update CatalogoRepositorio.java

--- repositorio/CatalogoRepositorio.java
+++ repositorio/CatalogoRepositorio.java
@@ -2,16 +2,36 @@ package repositorio;
 
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Iterator;
 import modelo.Catalogo;
 
 public class CatalogoRepositorio {
     private static List<Catalogo> catalogos = new ArrayList<>();
-
+    
     public void addCatalogo(Catalogo catalogo){
         catalogos.add(catalogo);
     }
-
-    /*for(int i = 0; i < catalogos.lenght; i++){
+    
+    public boolean nomeRepetido(String nome){
+        Iterator<Catalogo> iterator = catalogos.iterator();
         
-    }*/
+        while (iterator.hasNext()){
+            if (iterator.next().getNome() == nome){
+                return false
+            }
+        }
+    }
+    
+    public void consultaNome(){
+        while(iterator.hasNext()){
+            //compara os nomes pra ver se tem na lista
+            if ( == ){
+                break;
+            }
+            else{
+                System.out.println("Cd não encontrado");
+                return;
+            }
+        }
+    }
 }
